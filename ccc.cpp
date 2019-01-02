b870f5229414dc6211b23e0bec53c97074a64a65
spaidermanmfg <488434442@qq.com>
2019-01-02 15:53:17 +0800

    first commit

---
+++ ccc.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "*";
+	
+	int i;
+	for (i=1;i<=9;i++){
+		cout << "     " << i;	
+	} 
+	cout << "\n";
+	for (i=1;i<=9;i++){
+		cout << "\n" << i;
+	}
+	int j;
+	for (j=1;j<=9;j++){
+		if (j<i){
+			cout << "    " << j*i;
+		}
+	}
+}
