80910e240c01310857ad27607e2a98d236479e56
spaidermanmfg <488434442@qq.com>
2021-07-08 15:12:57 +0800

    添加换行符

--- ccc.cpp
+++ ccc.cpp
@@ -12,10 +12,12 @@ int main()
 	for (i=1;i<=9;i++){
 		cout << "\n" << i;
 	}
+    cout << "\n";
 	int j;
 	for (j=1;j<=9;j++){
 		if (j<i){
-			cout << "    " << j*i;
+			cout << "  hello  " << j*i;
+            
 		}
 	}
 }
